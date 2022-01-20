25d6e639cce7559d6b7bb1a9d127cf3d98900fa6
BenitaTB <62141042+BenitaTB@users.noreply.github.com>
2022-01-20 17:29:18 +0000

    Most recent UMI grouping scripts (still in progress)

--- 02_UMI_grouping.sh
+++ 02_UMI_grouping.sh
@@ -7,11 +7,11 @@ cd /camp/lab/znamenskiyp/home/shared/projects/turnerb_MAPseq/Sequencing/Processe
 awk "NR%2==0" BC5.txt | grep -v N | cut -b 1-50 |sort -nr > TUR4405A1processedBC5.txt
 
 mkdir indexes
-#build bowtie index library with your sequences
+
 ml Bowtie
 
-#take each sequence and alirgn to other sequences in the index library
+#take each sequence and align to other sequences in the index library (same library, so all to all)
 nl TUR4405A1processedBC5.txt | awk '{print ">" $1 "\n" $2}' > TUR4405A1processedBC5_fasta.txt; bowtie-build -q TUR4405A1processedBC5_fasta.txt indexes/BC5fasta; bowtie -v 3 -p 10 -f --best -a indexes/BC5fasta TUR4405A1processedBC5_fasta.txt bowtiealignment1_1.txt
 
-awk '{print $1}' bowtiealignment1_1.txt > bowtiealignment1_11.txt; awk '{print $3}' bowtiealignment1_1.txt > bowtiealignment1_3.txt
-#split output files into two files per index, one that is containing the counts of each unique sequnce, the other the unique sequences themselves.
+#take sequence line identifiers and alignments and put in one file
+awk '{print $1, $3}' bowtiealignment1_1.txt > bowtiealignment1_4.txt
