b7f1866c069bc73a5c928911735ba7e62616b181
BenitaTB <62141042+BenitaTB@users.noreply.github.com>
2022-04-11 11:50:33 +0100

    UMI grouping to function

--- Preprocessing_sequencing/02_UMI_grouping.py
+++ Preprocessing_sequencing/02_UMI_grouping.py
@@ -1,51 +1,66 @@
 import subprocess
 import os
 
-os.chdir('/camp/lab/znamenskiyp/home/shared/projects/turnerb_MAPseq/Sequencing/Processed_data/BRAC5676.1h/trial/unzipped1/barcodesplitter')
 
 directory = '/camp/lab/znamenskiyp/home/shared/projects/turnerb_MAPseq/Sequencing/Processed_data/BRAC5676.1h/trial/unzipped1/barcodesplitter'
 
-for barcodefile in os.listdir(directory):
-    if barcodefile.startswith("BC"):
-        newfile = 'TUR4405A1processed_%s' % barcodefile
-        with open(barcodefile, 'r') as bcreads, \
-            open(newfile, 'w') as target:
-            for il, sequence in enumerate(bcreads):
-                if il % 2== 1 and 'N' not in sequence:
-                    target.write(sequence[:50] + '\n')
-print('Processed each set of sample sequences for bowtie input')
-#now run bowtie to take each sequence and align to other sequences in the index library (same library, so all to all)
-if not os.path.isdir('indexes'):
-    os.mkdir('indexes')
-#make sequences in FASTA format for bowtie alignment
-for barcodefile in os.listdir(directory):
-    if barcodefile.startswith("TUR4405A1processed_"):
-        newfile = 'FASTA_%s' % barcodefile
-        with open(barcodefile, 'r') as bcreads, \
-            open(newfile, 'w') as target:
-            for il, sequence in enumerate(bcreads, start=1):
-                target.write('>' + str(il) + '\n' + sequence)
-verbose = 1
-if verbose:
-    print('Bowtie alignment starting', flush=True)
+def groupingUMI_restructure(directory):
 
-#Loop through each FASTA file and perform bowtie alignment.
-for barcodefile in os.listdir(directory):
-    if barcodefile.startswith("FASTA_TUR4405A1processed_BC"):
-        barcodenum = barcodefile.split('FASTA_TUR4405A1processed_', 1)[1]
-        suffix = '.txt'
-        barcodenum_nosuff = barcodenum[:-len(suffix)]
-        newfile = 'bowtiealignment_%s' % barcodenum
-        index = 'indexes/' + barcodenum_nosuff
-        library = subprocess.run(['bowtie-build', barcodefile, index], capture_output=True)
-        bowtie = subprocess.run(['bowtie', '-v', '3', '-p', '10', '-f', '--best', '-a', index, barcodefile, newfile], capture_output=True)
+    """
+    Function to take sequence reads that have been split into sample barcode files.
+    (i) Restructure the barcode splitter output so take every other line, cut to 50nt, take out N's
+    (ii) Make back into FASTA format
+    """
 
-#take first and third fields of bowtie alignments as output for next steps
-for alignment in os.listdir(directory):
-    if alignment.startswith("bowtiealignment_"):
-        fileoutput = 'out_%s' % alignment
-        with open (alignment,'r') as f, \
-            open(fileoutput, 'w') as target:
-            for x in f.readlines():
-                target.write(x.split('\t')[0] + ' ' + x.split('\t')[2] + '\n')
-print('Finished aligning UMI's)
+    os.chdir(directory)
+    if not os.path.isdir('indexes'):
+        os.mkdir('indexes')
+
+    for barcodefile in os.listdir(directory):
+        linenum = 1
+        if barcodefile.startswith("BC"):
+            newfile = 'processed_%s' % barcodefile
+            with open(barcodefile, 'r') as bcreads, \
+                open(newfile, 'w') as target:
+                for il, sequence in enumerate(bcreads):
+                    if il % 2== 1 and 'N' not in sequence:
+                        target.write('>' + str(linenum) + '\n' + sequence[:50] + '\n')
+                        linenum += 1
+    print('Processed each set of sample sequences for bowtie input')
+
+def UMI_bowtie(directory):
+
+    """
+    Function to run bowtie, taking each sequence in sample barcode library and align to other sequences in
+    the index library (same library, so all to all) by feeding into and running bowtie alignment to match UMI's
+
+    Args
+    directory = directory where sample splitting occurred
+    """
+    verbose = 1
+    if verbose:
+        print('Bowtie alignment starting', flush=True)
+
+    for barcodefile in os.listdir(directory):
+        if barcodefile.startswith("procesed_"):
+            barcodenum = barcodefile.split('processed_', 1)[1]
+            suffix = '.txt'
+            barcodenum_nosuff = barcodenum[:-len(suffix)]
+            newfile = 'bowtiealignment_%s' % barcodenum
+            index = 'indexes/' + barcodenum_nosuff
+            library = subprocess.run(['bowtie-build', barcodefile, index], capture_output=True)
+            bowtie = subprocess.run(['bowtie', '-v', '3', '-p', '10', '-f', '--best', '-a', index, barcodefile, newfile], capture_output=True)
+            print('bowtie alignment finished')
+
+def restructurebowtie(directory):
+    """
+    Function to take first and third fields of bowtie alignments as output for next steps
+    """
+    for alignment in os.listdir(directory):
+        if alignment.startswith("bowtiealignment_"):
+            fileoutput = 'out_%s' % alignment
+            with open (alignment,'r') as f, \
+                open(fileoutput, 'w') as target:
+                for x in f.readlines():
+                    target.write(x.split('\t')[0] + ' ' + x.split('\t')[2] + '\n')
+    print('Finished aligning UMI's)
