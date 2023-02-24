647edb974286e26f9ef30c46d5fe05d19be3781b
BenitaTB <Benita.Turner-Bridger@crick.ac.uk>
2023-02-24 15:38:45 +0000

    moved call function scripts to sep folder

--- Preprocessing_sequencing/Bash_scripts/temp_process.sh
+++ Preprocessing_sequencing/Bash_scripts/temp_process.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Job name
-#SBATCH --job-name=matching
+#SBATCH --job-name=final_big_o%j
 # Number of tasks in job script
 #SBATCH --ntasks=1
 #SBATCH --time=24:00:00
@@ -9,8 +9,8 @@
 
 
 # Notifications
-#SBATCH --output=/camp/home/turnerb/slurm_logs/BCBatch/180223/matching_%j.out
-#SBATCH --error=/camp/home/turnerb/slurm_logs/BCBatch/180223/matching_%j.err
+#SBATCH --output=/camp/home/turnerb/slurm_logs/BCBatch/180223/final_big_ones_%j.out
+#SBATCH --error=/camp/home/turnerb/slurm_logs/BCBatch/180223/final_big_ones_%j.err
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=benita.turner-bridger@crick.ac.uk
 # Main script that calls all the steps of the preprocessing
@@ -27,5 +27,5 @@ conda activate MAPseq_processing
 echo "Running MAPseq preprocessing"
 
 cd /camp/home/turnerb/home/users/turnerb/code/MAPseq_processing/Preprocessing_sequencing/Preprocessing_scripts
-python Call_functions_temp.py
+srun- python Call_functions_UMIchunk.py $1
 echo "Done"
